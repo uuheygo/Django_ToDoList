47a3e6babb99ea14accb268ef9ec36ced6f0613e
Lu Peng <pengluustc@gmail.com>
2015-02-09 12:29:16 -0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+	
+    def tearDown(self):
+        self.browser.quit()
+		
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Go to the home page
+        self.browser.get('http://localhost:8000')
+        
+        # Title and header have 'To-Do'
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        # Find the input box for a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        
+        # Enter a to-do item
+        inputbox.send_keys('Buy peacock feathers')
+        
+        # Page updates to add the to-do item
+        inputbox.send_keys(Keys.ENTER)
+        
+        table - self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        
+        # Another box asking for another to-do item
+        self.fail('Finished the test!')
 
-assert 'Django' in browser.title
+if __name__ == "__main__":
+    unittest.main()
+
