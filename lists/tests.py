1c41f09bce041d3cc54b450801e6d103feda8c92
Lu Peng <pengluustc@gmail.com>
2015-02-10 11:24:37 -0800

    Redirect after POST, and show all to-do items in template

--- lists/tests.py
+++ lists/tests.py
@@ -19,14 +19,39 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
         
     def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-		
-		response = home_page(request)
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
-		self.assertEqual(response.content.decode(), expected_html)
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        
+    def test_home_page_only_saves_items_when_necessay(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+        
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text = 'item1')
+        Item.objects.create(text = 'item2')
+        
+        request = HttpRequest()
+        response = home_page(request)
+        
+        self.assertIn('item1', response.content.decode())
+        self.assertIn('item2', response.content.decode())
 
 class ItemModelTest(TestCase):
     
@@ -44,5 +69,5 @@ class ItemModelTest(TestCase):
         
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item, 'The first list item')
-        self.assertEqual(second_saved_item, 'The second list item')
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'The second list item')
