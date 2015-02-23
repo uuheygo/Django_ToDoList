fa863ed4f0d0c4fbcc69453dd31e839b0b39cac5
Lu Peng <pengluustc@gmail.com>
2015-02-22 17:06:17 -0800

    created new model List and added list attribute to list item

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -18,16 +18,24 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = 'The first list item'
+        first_item.list = list_
         first_item.save()
         
         second_item = Item()
         second_item.text = 'The second list item'
+        second_item.list = list_
         second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
         
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -35,12 +43,15 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The second list item')
+        self.assertEqual(first_saved_item.list, list_)
         
 class ListViewTest(TestCase):
     def test_displays_all_items(self):
-        Item.objects.create(text='item1')
-        Item.objects.create(text='item2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item1', list = list_)
+        Item.objects.create(text='item2', list = list_)
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertContains(response, 'item1')
         self.assertContains(response, 'item2')
@@ -50,24 +61,11 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         
 class NewListTest(TestCase):
-    # def test_saving_a_POST_request(self):
-        # # request = HttpRequest()
-        # # request.method = 'POST'
-        # # request.POST['item_text'] = 'A new list item'
-        
-        # # response = home_page(request)
-        # # replace above code with simpler code
-        # self.client.post('/lists/new', data = {'item_text': 'A new list item'})
-        
-        # self.assertEqual(Item.objects.count(), 1)
-        # new_item = Item.objects.first()
-        # self.assertEqual(new_item.text, 'A new list item')
-        
-    # def test_redirects_after_POST(self):
-        # response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
-        
-        # self.assertEqual(response.status_code, 302)
-        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+    def test_saving_a_POST_request(self):
+        self.client.post('/lists/new', data = {'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
         
     def test_redirect_after_POST(self):
         response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
