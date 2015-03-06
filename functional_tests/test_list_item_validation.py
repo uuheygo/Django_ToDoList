65143719caa655605759d5334b5dd3b8234abe2e
Lu Peng <pengluustc@gmail.com>
2015-03-06 11:41:56 -0800

    Adject new list view to do model validation of empty item

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
         
         # Home page refreshes, and an error message saying the list item cannot be empty
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.test, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have an empty list item")
         
         # Enter a non-empty item and it works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
@@ -18,7 +18,7 @@ class ItemValidationTest(FunctionalTest):
         # Enter an empty item again, another error message shows update
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.test, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have an empty list item")
         
         # Fix it by filling some text in
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 
@@ -13,7 +14,12 @@ def view_list(request, list_id):
     
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text = request.POST['item_text'], list = list_)
+    item = Item.objects.create(text = request.POST['item_text'], list = list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
     
 def add_item(request, list_id):
